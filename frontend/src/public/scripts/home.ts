5321f12b76e7930c5345425b4aa9a9dfb91c425b
Nicholas Joannou <nicholasj@bbd.co.za>
2023-09-07 15:43:33 +0200

    Added search by functionality

--- frontend/src/public/scripts/home.ts
+++ frontend/src/public/scripts/home.ts
@@ -1,13 +1,40 @@
 const searchBar = document.getElementById("search-bar") as HTMLInputElement;
 const searchResults = document.getElementById("search-results") as HTMLDivElement;
 const searchButton = document.getElementById("search-button") as HTMLButtonElement;
+const categories = document.querySelectorAll<HTMLDivElement>(".category");
+
+let selectedCategory: string = "quote";
+
+function handleCategoryClick(event: MouseEvent) {
+  const clickedCategory = (event.target as HTMLDivElement).dataset.category;
+
+  if (clickedCategory !== selectedCategory) {
+    categories.forEach((category) => {
+      if (category.dataset.category === selectedCategory) {
+        category.classList.remove("selected");
+      }
+    });
+
+    (event.target as HTMLDivElement).classList.add("selected");
+    selectedCategory = clickedCategory || "quote";
+
+    searchBar.placeholder = `Enter ${selectedCategory} keywords`;
+  }
+
+  searchBar.value = "";
+  searchBar.placeholder = `Enter ${selectedCategory} keywords`;
+}
+
+categories.forEach((category) => {
+  category.addEventListener("click", handleCategoryClick);
+});
 
 function onResultPress(result: any) {
-  window.location.href = `results.html?quote=${result.Quote}`;
+  window.location.href = `results.html?quote=${result.Quote}&category=${selectedCategory}`;
 }
 
 searchButton.addEventListener('click', function () {
-  window.location.href = `results.html?searchText=${searchBar.value}`;
+  window.location.href = `results.html?searchText=${searchBar.value}&category=${selectedCategory}`;
 });
 
 let debounceTimeout: ReturnType<typeof setTimeout>;
@@ -23,8 +50,10 @@ searchBar.addEventListener('input', function (e) {
       searchResults.innerText = '';
       searchResults.classList.remove('visible');
 
+      const categorySelected = selectedCategory.charAt(0).toUpperCase() + selectedCategory.slice(1);
+
       if (query.length >= 3) {
-        const quoteEndpoint = `https://axnebcpkzz.eu-west-1.awsapprunner.com/quote/search/Quote/${query}`;
+        const quoteEndpoint = `https://axnebcpkzz.eu-west-1.awsapprunner.com/quote/search/${categorySelected}/${query}`;
         try {
           fetch(quoteEndpoint)
             .then((response) => response.json())
@@ -57,8 +86,9 @@ searchBar.addEventListener('input', function (e) {
                 searchResults.classList.add('visible');
               } else {
                 const noResults = document.createElement('div');
-                noResults.textContent = 'Quote not found';
+                noResults.textContent = 'No results';
                 searchResults.appendChild(noResults);
+                searchResults.classList.add('visible');
               }
             })
             .catch((error) => {
--- frontend/src/public/scripts/results.ts
+++ frontend/src/public/scripts/results.ts
@@ -1,10 +1,47 @@
 const searchBar_ = document.getElementById('search-bar') as HTMLInputElement;
 const resultsList = document.getElementById('results-list') as HTMLElement;
 
+const categoriesResultsPage = document.querySelectorAll<HTMLDivElement>(".category");
+
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 const quote = urlParams.get('quote');
 const searchText = urlParams.get('searchText');
+const categoryUrl = urlParams.get('category');
+
+let selectedCategoryResultsPage: string = categoryUrl || "quote";
+
+categoriesResultsPage.forEach((category) => {
+  if (category.dataset.category === selectedCategoryResultsPage) {
+    category.classList.add("selected");
+  } else {
+    category.classList.remove("selected");
+  }
+});
+
+function handleCategoryClickResultsPage(event: MouseEvent) {
+  const clickedCategory = (event.target as HTMLDivElement).dataset.category;
+
+  if (clickedCategory !== selectedCategoryResultsPage) {
+    categoriesResultsPage.forEach((category) => {
+      if (category.dataset.category === selectedCategoryResultsPage) {
+        category.classList.remove("selected");
+      }
+    });
+
+    (event.target as HTMLDivElement).classList.add("selected");
+    selectedCategoryResultsPage = clickedCategory || "quote";
+
+    searchBar_.placeholder = `Enter ${selectedCategoryResultsPage} keywords`;
+  }
+
+  searchForQuote(searchBar_.value);
+}
+
+categoriesResultsPage.forEach((category) => {
+  category.addEventListener("click", handleCategoryClickResultsPage);
+});
+
 
 if (!!quote) {
   searchBar_.value = quote;
@@ -20,17 +57,34 @@ searchBar_.addEventListener('input', debounce(() => {
   const inputValue = searchBar_.value;
 
   if (inputValue.length >= 3) {
+    resultsList.innerText = '';
     searchForQuote(inputValue);
+  } else {
+    resultsList.innerText = '';
   }
 }, 300));
 
 function searchForQuote(query: string) {
-  const category = "Quote";
+
+  const category = selectedCategoryResultsPage.charAt(0).toUpperCase() + selectedCategoryResultsPage.slice(1);
+
   fetch(`https://axnebcpkzz.eu-west-1.awsapprunner.com/quote/search/${category}/${query}`)
     .then(response => response.json())
     .then(data => {
-      displayResults(data);
-      console.log(JSON.stringify(data));
+      if (data.length > 0) {
+        displayResults(data);
+      } else {
+        if (resultsList.childElementCount === 0) {
+          const card = document.createElement('div');
+          card.className = 'result-card';
+
+          const noResults = document.createElement('p');
+          noResults.className = 'quote';
+          noResults.textContent = "No results";
+          card.appendChild(noResults);
+          resultsList.appendChild(card);
+        }
+      }
     })
     .catch(error => {
       console.error('Quote search error:', error);
